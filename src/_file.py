bf2f89f49f1b3cb1bca32dc9d0ee2375d48d8428
Pablo Benito González <pablobenitogonzalez@gmail.com>
2025-07-29 15:54:14 +0200

    Remove ed2klinks folder

--- src/_file.py
+++ src/_file.py
@@ -1,11 +1,7 @@
-import datetime
 import os
 import re
 
 READING_PATH = '../reading'
-ED2K_LINKS_PATH = '../ed2klinks'
-ED2K_LINKS_SIZE = 50
-
 
 class FileManager:
     @staticmethod
@@ -17,14 +13,3 @@ class FileManager:
             if len(topic) > 0:
                 topics.append(topic)
         return topics
-
-    @staticmethod
-    def write(topic: int, ed2ks: list) -> int:
-        file_datetime = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
-        groups = [ed2ks[i:i + ED2K_LINKS_SIZE] for i in range(0, len(ed2ks), ED2K_LINKS_SIZE)]
-        for gid, group in enumerate(groups):
-            ed2k_links_file = f'{ED2K_LINKS_PATH}/[{topic}]_{file_datetime}_{str(gid).zfill(3)}.txt'
-            with open(ed2k_links_file, 'w', encoding='utf-8') as f:
-                for eid, ed2k in enumerate(group):
-                    f.write(f'{ed2k[0]}\n')
-        return len(groups)
